25a3cb0d2f8bb8bdfe99cc5c28e2e4d192a2fd0a
AlessandroMartorelli <alessandromartorelli62@yahoo.com>
2025-05-14 15:30:06 +0200

    test1

---
+++ CLSBlutalkohol.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blutalkohol
+{
+    internal class CLSBlutalkohol
+    {
+        //Attributes
+        private double ergebnis = 0.0;
+        private double faktor = 0.7;
+        // Properties 
+        public double Masse { get; set; }
+        public double Volumen { get; set; }
+        public double Prozent { get; set; }
+        public string Geschlecht { get; set; }
+
+        //Methods 
+        public double Berechnen() 
+        {
+            if (Geschlecht == "m") faktor = 0.7;
+            else if (Geschlecht == "f") faktor = 0.6;
+            else faktor = 0.8; 
+
+            double A = Volumen*(Prozent/100)*0.8;
+            ergebnis = A / (Masse * faktor);
+            return ergebnis;
+            
+        
+        
+        
+        
+        
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blutalkohol
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ende_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        CLSBlutalkohol ba= new CLSBlutalkohol();
+        private void btnBerechnen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double result = 0.0;
+                ba.Masse = double.Parse(txtGewicht.Text.Replace(".",","));
+                ba.Volumen = double.Parse(txtMenge.Text.Replace(".", ","));
+                ba.Prozent = double.Parse(txtProzent.Text.Replace(".", ","));
+                if (rbMann.IsChecked == true) ba.Geschlecht = "m";
+                else if (rbFrau.IsChecked == true) ba.Geschlecht = "f";
+                else ba.Geschlecht = "k";
+                result = Math.Round(ba.Berechnen(),2); 
+                txtBlutwert.Text= result.ToString();
+            }
+            catch
+            {
+                txtBlutwert.Text = "---Error---";
+                MessageBox.Show("Die Eingabeparrameter überprüfen!!!!!!!","Eingabefehler",
+                    MessageBoxButton.OK,MessageBoxImage.Error);
+             
+
+
+            }
+            
+               
+
+            
+            
+        }
+    }
+}
